1a6696aac5f55f73e42a4cec2cf6a81e5d5b0ad5
Gilad <Gilad0635@gmail.com>
2023-04-06 19:46:53 +0300

    Change

--- script.js
+++ script.js
@@ -7,12 +7,24 @@ var hintText = document.querySelector('.hint-text');
 // Hide the hint text initially
 hintText.style.display = 'none';
 
-// Add a click event listener to the button
-hintButton.addEventListener('click', function() {
-	// Toggle the visibility of the hint text
-	if (hintText.style.display === 'none') {
-		hintText.style.display = 'block';
-	} else {
-		hintText.style.display = 'none';
-	}
+// Get DOM elements
+const Easy = document.getElementById('Easy');
+const Hard = document.getElementById('Hard');
+const container = document.getElementById('container');
+
+// Store the original riddle and hint text content
+const originalRiddle = document.querySelector('.riddle-title').textContent;
+const originalHint = document.querySelector('.hint-text').textContent;
+
+// Add click event listeners to buttons
+Easy.addEventListener('click', () => {
+  // Reset riddle and hint text content to original values
+  document.querySelector('.riddle-title').textContent = originalRiddle;
+  document.querySelector('.hint-text').textContent = originalHint;
+});
+
+Hard.addEventListener('click', () => {
+  // Change content to "Hard" for hard mode
+  document.querySelector('.riddle-title').textContent = "This is a new riddle for Hard mode!";
+  document.querySelector('.hint-text').textContent = "This is a new hint text for Hard mode!";
 });
